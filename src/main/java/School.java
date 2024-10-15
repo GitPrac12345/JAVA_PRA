86cb486739f7545c37a6e9910224a05077349565
GitPrac12345 <gitgitdemoprac@gmail.com>
2024-10-15 20:48:14 +0530

    classes and objects

---
+++ src/main/java/School.java
@@ -0,0 +1,15 @@
+import java.sql.SQLOutput;
+
+public class School {
+String name;
+String address;
+String principal;
+int teacherCount;
+int studentCount;
+
+public void displayResult(){
+    System.out.println("Every students passed");
+
+}
+
+}
