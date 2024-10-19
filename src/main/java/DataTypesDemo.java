b4c1de4b771d35deca3de4d5bd75981679ba847e
GitPrac12345 <gitgitdemoprac@gmail.com>
2024-10-19 13:09:16 +0530

    Day2

---
+++ src/main/java/DataTypesDemo.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class DataTypesDemo {
+    public static void main(String[] args) {
+        byte a1 = (byte) 256;
+        short s= 12344;
+        int i=787656555;
+        long l=87475849454L;
+        float w= 123.65f;
+        double d=123455.785765;
+        Boolean isValid=true;
+        char c='A';
+       // int ascVal=c;
+        System.out.println("ascVal is:"+(int)c);
+
+        int v='A';
+        char v1=(char) (v+3);
+        System.out.println(v1);
+        String str="anak";
+        str.toUpperCase();
+        System.out.println(str.toUpperCase());
+
+
+
+}
+
+}
